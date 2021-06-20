e8fdd737cd2d3e097240c3c4ef5f2eb3b5012114
sam <74613721+Tawedzerwa80@users.noreply.github.com>
2021-06-20 19:53:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/*declaeration of variables to grab our user inputs(for both password & Email Address)*/
+var emailinput = document.getElementsByClassName("loggin-mail");
+var passwordinput = document.getElementsByClassName("loggin-pass");
+var button = document.getElementsByClassName("button");
+
+/*beggining of the function to check the entered username& password*/
+
+function PassInputLength (){
+    return Input.value.Length
+
+}
+
+function EmailInputLength (){
+    return Input.value.Length
+
+}
